c949e81ae9b712e91916d679492730538f19d3db
Lee Martin <leefredmart@gmail.com>
2019-11-04 12:41:11 -0500

    first commit

---
+++ playground/arrow-function.js
@@ -0,0 +1,17 @@
+const square = x => x * x
+console.log(square(9))
+
+const user = {
+    name: 'Lee Martin',
+    sayHi: () => {
+        console.log(arguments)
+        console.log(`Hi. I'm ${this.name}`)
+    },
+    sayHiAlt () {
+        console.log(arguments)
+        console.log(`Hi. I'm ${this.name}`)
+    }
+}
+
+user.sayHi()
+user.sayHi(1, 2, 3)
---
+++ playground/debugging.js
@@ -0,0 +1,12 @@
+const person = {
+    name: 'Lee'
+}
+
+person.age = 25
+person.name = 'Mikey'
+
+debugger
+
+person.size = '8 inches'
+
+console.log(person)
